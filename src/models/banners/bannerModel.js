f12dbccff7006fc40260e159ec602eebe44ab12a
surajexpo <surajexpo.dev@gmail.com>
2025-12-22 22:22:12 +0530

    update field in banner section

--- src/models/banners/bannerModel.js
+++ src/models/banners/bannerModel.js
@@ -1,18 +1,25 @@
-const mongoose =require('mongoose');
-const bannerSchema= new mongoose.Schema({
-    title:
-{
-    type:String,
-    required:true,
-  
-},
-    image:{
-    type:String,
-    required:true,
+const mongoose = require("mongoose");
+const bannerSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    description:{
-    type:String,
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    redirectUrl: {
+      type: String,
+    },
+    isActive:{
+        type:Boolean
     }
-});
-const banners=mongoose.model('Banners',bannerSchema);
-module.exports=banners;
+  },
+  { timestamps: true }
+);
+const banners = mongoose.model("Banners", bannerSchema);
+module.exports = banners;
